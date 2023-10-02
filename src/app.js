2908ee2d9b23df0a1386dad98dc1c82833902a0c
tomas-maker2 <tbolotnicoff@gmail.com>
2023-10-02 19:38:18 -0300

    entrega

--- src/app.js
+++ src/app.js
@@ -8,6 +8,16 @@ import mongoose from 'mongoose';
 import { Server } from 'socket.io';
 import { mensajeModel } from './dao/models/mensajes.model.js';
 
+// IMPORTAMOS MONGO Y SESSION
+import MongoStore from 'connect-mongo';
+import session from 'express-session';
+
+// IMPORTAR VIEWSROUTER
+import viewRouter from './routes/viewsRouter.js'
+
+// IMPORT USERROUTER
+import userRouter from './routes/userRouter.js'
+
 mongoose.connect(
   'mongodb+srv://tomasmaker2:topper10@cluster0.na8mlhz.mongodb.net/?retryWrites=true&w=majority'
 )
@@ -24,6 +34,18 @@ app.set('views', './src/views')
 app.set('view engine', 'handlebars');
 app.use(express.static('./src/public'))
 
+// USAMOS SESSION
+
+app.use(session({
+  store: MongoStore.create({
+    mongoUrl: 'mongodb+srv://tomasmaker2:topper10@cluster0.na8mlhz.mongodb.net/?retryWrites=true&w=majority',
+    ttl:15
+  }),
+  secret: 'docinecub',
+  resave: false,
+  saveUninitialized: false,
+}))
+
 app.get('/', (req,res) => {
   res.render('home', {pageTitle: 'Pagina de Inicio'})
 })
@@ -36,6 +58,11 @@ app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 app.use('/api/chat' , chatRouter );
 app.use('/views', viewsRouter)
+
+// USE VIEWROUTER
+app.use('/api', userRouter)
+app.use('/', viewRouter);
+
 
 
 
